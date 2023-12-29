976f42407278a67e5c5ca5b24e5928184a264a88
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-12-29 15:37:17 +0100

    added more data

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-Welcome! <br />
-City : Toulouse <br />
-Current weather : clear sky <br />
-Temperature is 11.88°C. <br />
-Last refresh at jeudi 28 décembre à 13:33 UTC.
+## Welcome! 👋😎
+
+🌀 Last refresh at Friday, December 29 at 15:35 GMT+1. <br /><br />
+<ul>
+    <li>Weather in Toulouse : Clouds</li>
+    <li>Current temperature : 12.93°C.</li>
+    <li>Humidity : 76%.</li>
+    <li>Wind speed : 3.6 km/h.</li>
+    <br />
+    <li>Today min / max temperature : 🔵 12.12°C  🔴 13.2°C</li>
+    <li>Today sunrise / sunset : 🌞 08:27  🌚 17:24</li>
+    <li>Still 1:48 before sunset and 2 hours before Friday night 💅.</li>
+</ul>
--- index.js
+++ index.js
@@ -8,19 +8,21 @@ import fetch from "node-fetch";
 import Mustache from "mustache";
 import fs from "fs";
 
+const DATE_OPTIONS = {
+  weekday: "long",
+  month: "long",
+  day: "numeric",
+  hour: "2-digit",
+  hour12: false,
+  minute: "2-digit",
+  timeZoneName: "short",
+  timeZone: "Europe/Paris",
+};
 const MUSTACHE_MAIN_DIR = "./main.mustache";
 let DATA = {
   name: "Thomas",
   location: "Toulouse",
-  date: new Date().toLocaleDateString("fr-FR", {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    timeZoneName: "short",
-    timeZone: "Europe/London",
-  }),
+  date: new Date().toLocaleDateString("en-EN", DATE_OPTIONS),
 };
 
 async function setWeatherInformation() {
@@ -29,10 +31,32 @@ async function setWeatherInformation() {
       `https://api.openweathermap.org/data/2.5/weather?q=${DATA.location}&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
     );
     const json = await response.json();
+    console.log(json);
     DATA.temperature = json.main.temp;
-    DATA.weather = json.weather[0].description;
+    DATA.temp_min = json.main.temp_min;
+    DATA.temp_max = json.main.temp_max;
+    DATA.humidity = json.main.humidity;
+    DATA.wind_speed = json.wind.speed;
+    DATA.weather = json.weather[0].main;
+    DATA.sunrise = new Date(json.sys.sunrise * 1000).toLocaleTimeString("en-EN", {
+      timeZone: DATE_OPTIONS.timeZone,
+      minute: DATE_OPTIONS.minute,
+      hour: DATE_OPTIONS.hour,
+      hour12: DATE_OPTIONS.hour12,
+    });
+    DATA.sunset = new Date(json.sys.sunset * 1000).toLocaleTimeString("en-EN", {
+      timeZone: DATE_OPTIONS.timeZone,
+      minute: DATE_OPTIONS.minute,
+      hour: DATE_OPTIONS.hour,
+      hour12: DATE_OPTIONS.hour12,
+    });
+    DATA.time_before_sunset = `${Math.floor((json.sys.sunset * 1000 - Date.now()) / 1000 / 60 / 60)}:${Math.floor(
+      ((json.sys.sunset * 1000 - Date.now()) / 1000 / 60) % 60
+    )}`;
+    DATA.time_before_friday = Math.floor(getTimeUntilNextFriday18h());
+    DATA.weather_icon_url = `http://openweathermap.org/img/w/${json.weather[0].icon}.png`;
   } catch (error) {
-    console.log(error);
+    throw error;
   }
 }
 
@@ -42,6 +66,19 @@ function generateReadMe() {
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync("README.md", output);
   });
+}
+
+function getTimeUntilNextFriday18h() {
+  const now = new Date();
+  const dayOfWeek = now.getDay();
+  const daysUntilFriday = (5 - dayOfWeek + 7) % 7;
+  const nextFriday = new Date(now.getTime() + daysUntilFriday * 24 * 60 * 60 * 1000);
+  nextFriday.setHours(18, 0, 0, 0);
+
+  const timeRemainingInMillis = nextFriday.getTime() - now.getTime();
+  const timeRemainingInHours = timeRemainingInMillis / (60 * 60 * 1000);
+
+  return timeRemainingInHours;
 }
 
 async function action() {
